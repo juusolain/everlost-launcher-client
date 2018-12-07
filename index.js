5b12d18e1ec28cfd34add0fedcc1011c1c3c96d5
jusola <juusolain@gmail.com>
2018-12-07 20:58:28 +0200

    Some build changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let win
 autoUpdater.checkForUpdatesAndNotify()
 
 function createWindow() {
-  win = new BrowserWindow({ width: 1440, height: 810, autoHideMenuBar: true, backgroundColor: "#2C2F33", show: false, resizable: false, frame: false, icon:'build/icon.png'});
+  win = new BrowserWindow({ width: 1440, height: 810, autoHideMenuBar: true, backgroundColor: "#2C2F33", show: false, resizable: false, frame: false, icon:'build/icon.png', webPreferences: {devTools: false}});
 
   win.loadFile('static/index.html');
 
